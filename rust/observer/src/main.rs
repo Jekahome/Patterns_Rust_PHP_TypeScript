b31a4bce4cc702d11fa0b3c9e9600a77c2e82224
Eugene Yaroshenko <yaroshjeka@gmail.com>
2020-02-22 17:18:47 +0200

    observers rust

--- rust/observer/src/main.rs
+++ rust/observer/src/main.rs
@@ -1,10 +1,11 @@
 //use std::sync::{RwLock, RwLockWriteGuard};
 use std::rc::Rc;
 use std::collections::HashMap;
-use std::option::Option;
 use uuid::Uuid;
 use std::cell::{RefCell, Ref};
-use rand::prelude::*;
+use rand::{thread_rng, Rng};
+use rand::distributions::Alphanumeric;
+
 
 //use std::borrow::{BorrowMut, Borrow}; не работает если подключить
 
@@ -23,9 +24,18 @@ use rand::prelude::*;
 // возможность выборочно брать изменчивое/не изменчивое заимствование (RefCell),
 // для пооддержки определенного интерфейса Box<dyn ...>
 
+
+fn rand_string() -> String {
+         thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(30)
+        .collect()
+}
+
+
 trait SubjectImpl
 {
-    fn register_observer(&mut self,obs:Rc<RefCell<Box<dyn ObserverImpl>>>) -> Option<Uuid>;
+    fn register_observer(&mut self,obs:Rc<RefCell<Box<dyn ObserverImpl>>>) -> bool;
     fn remove_observer(&mut self,key:Uuid);
     fn notify_observers(&mut self);
     fn set_measurements(&mut self, temperature:f32, humidity:f32, pressure:i32);// фейковая загрузка
@@ -49,6 +59,7 @@ trait ObserverImpl
     fn remove_observer(&mut self);
     fn get_subscription(&self)->bool;
 }
+
 
 trait DisplayElementImpl
 {
@@ -111,52 +122,48 @@ impl WeatherData{
         self.set_changed(); // Если новые данные не значительные можно не оповещать
         self.notify_observers();
     }
-    // удалить отписавшихся подписчиков
+    // удалить отписавшихся подписчиков по признаку отписку
     fn update_active_observer(&mut self){
-        self.observers.retain(|_, v| v.borrow().get_subscription());
+        self.observers.retain(|_, v| if v.borrow().get_subscription(){
+            return true;
+        }else{
+            println!("<<<Отписка>>> {:?}:", v.borrow().get_key());
+            println!("-----------------------------------");
+            return false;
+        });
     }
 
 }
 
 impl SubjectImpl for WeatherData{
-    fn register_observer(&mut self,obs:Rc<RefCell<Box<dyn ObserverImpl>>>) -> Option<Uuid>{
+    fn register_observer(&mut self,obs:Rc<RefCell<Box<dyn ObserverImpl>>>)->bool{
         if !self.observers.contains_key(&obs.borrow().get_key()){
-            let key = Uuid::new_v5(&Uuid::NAMESPACE_OID,"Observer".as_bytes());
+
+            let key = Uuid::new_v5(&Uuid::NAMESPACE_OID,rand_string().as_bytes());
+            obs.borrow_mut().set_key(key);
             self.observers.insert(key, obs);
-           return Some(key);
+            return true;
         }
-        None
+        false
     }
+    // удалить отписавшегося подписчика напрямую по Uuid
     fn remove_observer(&mut self,key:Uuid){
         if self.observers.contains_key(&key){
+            println!("Отписка {:?}:", &key);
+            println!("-----------------------------------");
             self.observers.remove(&key);
-            println!("key {:?} remove_observer",&key);
-            //self.observers.retain(|x| x != &obs);
         }
     }
     fn notify_observers(&mut self){
         self.update_active_observer();
         if self.has_changed() {
-            /*let mut obs = &mut self.observers;
-            obs.iter_mut().map(|el|{
-                el.update();
-            });*/
-
-            /*for el in self.observers.values_mut() {
-                //&mut Rc<RefCell<Box<dyn ObserverImpl>>>
-               el.borrow_mut().update();
-            }*/
+            //for el in self.observers.values_mut() {
+            //   el.borrow_mut().update(self.data.borrow());
+            //}
             for (_, el) in self.observers.iter_mut() {
                 el.borrow_mut().update(self.data.borrow());
-
             }
-            /*let iter = &self.observers.iter_mut();
-            iter.map(| el|{
-                el.update(&self);
-            });*/
-            /*for mut el in self.observers.into_iter(){
-                el.update(&self);
-            }*/
+
             self.clear_changed();
         }
     }
@@ -173,8 +180,8 @@ impl SubjectImpl for WeatherData{
     }
 }
 
-
-
+// Наблюдатели
+//--------------------------------------------------------------------------------------------------
 #[derive(Clone)]
 struct CurrentConditionsDisplay{
     temperature:f32,
@@ -188,8 +195,6 @@ impl CurrentConditionsDisplay{
         CurrentConditionsDisplay{temperature:0.0,humidity:0.0,pressure:0 ,key:Default::default(),subscription:false}
     }
 }
-
-
 
 impl ObserverImpl for CurrentConditionsDisplay{
     fn update(&mut self,weather_data:Ref<Box<dyn DataImpl>>){
@@ -210,7 +215,7 @@ impl ObserverImpl for CurrentConditionsDisplay{
     }
     fn remove_observer(&mut self){
         //self.weatherData.upgrade().unwrap().borrow_mut().remove_observer( self.get_key());
-        self.subscription = false; //ПРИВЕСТИ В ПОРЯДОК СПОСОБ ОТКАЗА ОТ ПОДПИСКИ
+        self.subscription = false;
     }
     fn get_subscription(&self)->bool{
         self.subscription
@@ -224,6 +229,113 @@ impl DisplayElementImpl for CurrentConditionsDisplay{
         println!("-----------------------------------");
     }
 }
+//--------------------------------------------------------------------------------------------------
+
+struct StatisticsDisplay {
+    max_temperature:f32,
+    min_temperature:f32,
+    sum_temperature:f32,
+    num_readings:i32,
+    key:Uuid,
+    subscription:bool
+}
+
+impl StatisticsDisplay {
+    fn constructor() -> Self {
+        StatisticsDisplay{max_temperature:0.0,min_temperature:0.0,sum_temperature:0.0 ,num_readings:0,key:Default::default(),subscription:false}
+    }
+}
+impl ObserverImpl for StatisticsDisplay{
+    fn update(&mut self,weather_data:Ref<Box<dyn DataImpl>>){
+        if self.subscription {
+
+            let temp_temperature = weather_data.get_temperature();
+            self.sum_temperature += temp_temperature;
+            self.num_readings+=1;
+            if temp_temperature > self.max_temperature {
+                self.max_temperature = temp_temperature;
+            }
+
+            if temp_temperature < self.min_temperature {
+                self.min_temperature = temp_temperature;
+            }
+
+            self.display();
+        }
+    }
+    fn get_key(&self) -> Uuid{
+        self.key
+    }
+    fn set_key(&mut self,key:Uuid){
+        self.key = key;
+        self.subscription = true;
+    }
+    fn remove_observer(&mut self){
+        self.subscription = false;
+    }
+    fn get_subscription(&self) -> bool{
+        self.subscription
+    }
+}
+impl DisplayElementImpl for StatisticsDisplay{
+    fn display(&self){
+        println!("Статистика {:?}:",self.get_key());
+        println!("Avg/Max/Min temperature = {:?}/{:?}/{:?}",(self.sum_temperature / self.num_readings as f32), self.max_temperature,self.min_temperature);
+        println!("-----------------------------------");
+    }
+}
+
+
+//--------------------------------------------------------------------------------------------------
+struct ForecastDisplay{
+    current_pressure:i32,
+    last_pressure:i32,
+    key:Uuid,
+    subscription:bool
+}
+impl ForecastDisplay{
+    fn constructor() -> Self {
+        ForecastDisplay{current_pressure:0,last_pressure:0,key:Default::default(),subscription:false}
+    }
+}
+
+impl ObserverImpl for ForecastDisplay{
+    fn update(&mut self,weather_data:Ref<Box<dyn DataImpl>>){
+        if self.subscription {
+            self.last_pressure = self.current_pressure;
+            self.current_pressure = weather_data.get_pressure();
+
+            self.display();
+        }
+    }
+    fn get_key(&self) -> Uuid{
+        self.key
+    }
+    fn set_key(&mut self,key:Uuid){
+        self.key = key;
+        self.subscription = true;
+    }
+    fn remove_observer(&mut self){
+        self.subscription = false;
+    }
+    fn get_subscription(&self) -> bool{
+        self.subscription
+    }
+}
+impl DisplayElementImpl for ForecastDisplay{
+    fn display(&self){
+        println!("Прогноз {:?}:",self.get_key());
+        if self.current_pressure > self.last_pressure {
+            println!("Скоро будет улучшение погоды!");
+        } else if self.current_pressure == self.last_pressure {
+            println!("Усиление погодных условий");
+        } else if self.current_pressure < self.last_pressure {
+            println!("Остерегайтесь прохладной, дождливой погоды");
+        }
+        println!("-----------------------------------");
+    }
+}
+//--------------------------------------------------------------------------------------------------
 
 
 
@@ -232,24 +344,47 @@ fn main(){
     let weather_data:Box<dyn SubjectImpl> =  Box::new(WeatherData::constructor(data));
     let weather_data_rc:Rc<RefCell<Box<dyn SubjectImpl>>> =  Rc::new(RefCell::new(weather_data));
 
-
-    let current_conditions_display = CurrentConditionsDisplay::constructor();//
+    // CurrentConditionsDisplay --------------------------------------------------------------------
+    let current_conditions_display = CurrentConditionsDisplay::constructor();
     let current_conditions_display_rc =  Rc::new(RefCell::new(Box::new(current_conditions_display.clone() ) as Box<dyn ObserverImpl> ));
+    weather_data_rc.borrow_mut().register_observer( Rc::clone(&current_conditions_display_rc));
 
 
-    //println!("key default:{:?}",conRc.borrow().get_key());
-    let key:Option<Uuid> = weather_data_rc.borrow_mut().register_observer( Rc::clone(&current_conditions_display_rc));
-    if let Some(key) = key{
-        //println!("key:{:?}",key);
-        current_conditions_display_rc.borrow_mut().set_key(key);
-    }
-    //println!("key new:{:?}",conRc.borrow().get_key());
+    // StatisticsDisplay ---------------------------------------------------------------------------
+    let statistics_display = StatisticsDisplay::constructor();
+    let statistics_display_rc =  Rc::new(RefCell::new(Box::new(statistics_display) as Box<dyn ObserverImpl> ));
+    weather_data_rc.borrow_mut().register_observer( Rc::clone(&statistics_display_rc));
 
-     let mut rng = thread_rng();
-    weather_data_rc.borrow_mut().set_measurements(rng.gen_range(10.0, 100.0),rng.gen_range(10.0, 100.0),rng.gen_range(400, 800));
+    // ForecastDisplay -----------------------------------------------------------------------------
+    let forecast_display = ForecastDisplay::constructor();
+    let forecast_display_rc =  Rc::new(RefCell::new(Box::new(forecast_display) as Box<dyn ObserverImpl> ));
+    weather_data_rc.borrow_mut().register_observer( Rc::clone(&forecast_display_rc));
 
+
+    // Заполнение данными и оповещение
+    let mut rng = thread_rng();
+    //№1
+    weather_data_rc.borrow_mut()
+        .set_measurements(
+            rng.gen_range(10.0, 100.0),
+            rng.gen_range(10.0, 100.0),
+            rng.gen_range(400, 800));
+
+    //№2
+    weather_data_rc.borrow_mut()
+        .set_measurements(
+            rng.gen_range(10.0, 100.0),
+            rng.gen_range(10.0, 100.0),
+            rng.gen_range(400, 800));
+
+    //отписка
     current_conditions_display_rc.borrow_mut().remove_observer();
-    weather_data_rc.borrow_mut().set_measurements(rng.gen_range(10.0, 100.0),rng.gen_range(10.0, 100.0),rng.gen_range(400, 800));
 
+    //№3
+    weather_data_rc.borrow_mut()
+        .set_measurements(
+            rng.gen_range(10.0, 100.0),
+            rng.gen_range(10.0, 100.0),
+            rng.gen_range(400, 800));
 
 }
